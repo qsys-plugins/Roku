f61edb39447ec5ba3e7e9af0ea619915cff5442a
Amanda Abate <Amanda.Abate@qsc.com>
2019-05-29 11:48:11 -0600

    updated guid

--- info.lua
+++ info.lua
@@ -2,7 +2,7 @@ PluginInfo =
 {
   Name = "Roku Control",
   Version = "0.0.0.8",
-  Id = "COMPILEDPLUGIN-RokuControl",
+  Id = "771293da-359c-48ec-af73-438fb677547b",
   Description = "Roku Control From QSD",
   ShowDebug = true,
   InstallPath = ""
